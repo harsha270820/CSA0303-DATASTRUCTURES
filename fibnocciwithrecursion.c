a81a7051f861a89a8e4b418dd63708c615741c67
harsha270820 <122298047+harsha270820@users.noreply.github.com>
2023-02-02 13:38:57 +0530

    Add files via upload

---
+++ fibnocciwithrecursion.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void fibnocci(){
+    int n1=0,n2=1,n3,num,i;
+    printf("enter the number\n");
+    scanf("%d",&num);
+    for(i=0;i<num;i++){
+        n3=n1+n2;
+        n1=n2;
+        n2=n3;
+        printf("%d\t",n1,n2);
+    }
+}
+int main(){
+    fibnocci();
+return 0;
+}
